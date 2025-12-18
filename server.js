e28269f187944cc41f9ea3e1b1b3b767e99fd13c
Ashish Morwal <ashishmorwal001@gmail.com>
2025-12-18 21:02:04 +0530

    update server.js

--- server.js
+++ server.js
@@ -1,49 +1,3 @@
-// require("dotenv").config();
-// const express = require("express");
-// const cors = require("cors");
-// const connectDB = require("./config/db");
-
-// const authRoutes = require("./routes/authRoutes");
-// const sessionRoutes = require("./routes/sessionRoutes");
-// const questionRoutes = require("./routes/questionRoutes");
-// const { protect } = require("./middlewares/authMiddleware");
-// const {
-//   generateInterviewQuestions,
-//   generateConceptExplanation,
-// } = require("./controllers/aiController");
-
-// const app = express();
-
-// // Middleware to handle CORS
-// app.use(
-//   cors({
-//     origin: "*",
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//   })
-// );
-
-// connectDB();
-
-// app.get("/api/health", (req, res) => {
-//   res.json({ status: "ok" });
-// });
-
-// // Middleware
-// app.use(express.json());
-
-// // Routes
-// app.use("/api/auth", authRoutes);
-// app.use("/api/sessions", sessionRoutes);
-// app.use("/api/questions", questionRoutes);
-
-// app.use("/api/ai/generate-questions", protect, generateInterviewQuestions);
-// app.use("/api/ai/generate-explanation", protect, generateConceptExplanation);
-
-// // Start Server
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
@@ -60,7 +14,7 @@ const {
 
 const app = express();
 
-// ✅ CORS
+// CORS
 const corsOptions = {
   origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
@@ -89,4 +43,3 @@ app.use("/api/ai/generate-explanation", protect, generateConceptExplanation);
 // Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
